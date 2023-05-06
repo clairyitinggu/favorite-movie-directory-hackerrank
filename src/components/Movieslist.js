07d20e997ce231f3d3115e6fdfd62106bb6e9c9e
Malik Mubashir <malikmubashir025@gmail.com>
2023-05-06 16:50:20 +0500

    if this commit gets merged it will fix errors in movielist

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -1,32 +1,29 @@
 import React from "react";
 
-function Movieslist(props) {
-  const { movies } = props;
+function MovieList({ movies }) {
+  if (!movies || movies.length === 0) {
+    return null; // Render nothing if there are no movies to display
+  }
 
   return (
-    <section>
-      <ul className='styled w-100 pl-0' data-testid='moviesList'>
-        {movies.map((movie, index) => (
-          <li
-            key={index}
-            className='flex slide-up-fade-in justify-content-between'
-            style={{ borderBottom: "2px solid var(--primary-color)" }}
-          >
-            <div className='layout-column w-40'>
-              {/* use this header for movie name */}
-              <h3 className='my-3'>{movie.name}</h3>
-              {/* use this paragraph for movie ratings, for example: 'Ratings: 88/100' */}
-              <p className='my-0'>Ratings: {movie.ratings}/100</p>
+    <ul className='styled w-100 pl-0' data-testid='moviesList'>
+      {movies.map((movie) => (
+        <li key={movie.id} className='slide-up-fade-in py-10'>
+          <div className='flex-1'>
+            <div className='font-weight-bold text--medium'>{movie.name}</div>
+            <div>
+              <span className='font-weight-bold'>Rating:</span>{" "}
+              <span>{movie.ratings}</span>
             </div>
-            <div className='layout-row my-auto mr-20'>
-              {/* use this paragraph for movie duration, for example: '2.5 Hrs' */}
-              <p className='justify-content-end'>{movie.duration}</p>
+            <div>
+              <span className='font-weight-bold'>Duration:</span>{" "}
+              <span>{movie.duration}</span>
             </div>
-          </li>
-        ))}
-      </ul>
-    </section>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export default Movieslist;
+export default MovieList;
