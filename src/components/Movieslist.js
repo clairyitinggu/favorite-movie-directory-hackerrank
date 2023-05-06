e485a11fe0f31001f529d3497e34cc0cfe93dcc1
Malik Mubashir <malikmubashir025@gmail.com>
2023-05-06 16:56:25 +0500

    if this commit gets merged it will fix errors in movielist

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function MovieList({ movies }) {
+function MovieList({ movies, noResult }) {
   if (!movies || movies.length === 0) {
-    return null; // Render nothing if there are no movies to display
+    return <div data-testid='noResult'>{noResult}</div>; // Render no result message if there are no movies to display
   }
 
   return (
@@ -13,11 +13,15 @@ function MovieList({ movies }) {
             <div className='font-weight-bold text--medium'>{movie.name}</div>
             <div>
               <span className='font-weight-bold'>Rating:</span>{" "}
-              <span>{movie.ratings}</span>
+              <span>{movie.rating}/100</span>
             </div>
             <div>
               <span className='font-weight-bold'>Duration:</span>{" "}
-              <span>{movie.duration}</span>
+              <span>
+                {movie.duration.includes("m")
+                  ? `${Number(movie.duration.slice(0, -1)) / 60} Hrs`
+                  : `${movie.duration} Hrs`}
+              </span>
             </div>
           </div>
         </li>
